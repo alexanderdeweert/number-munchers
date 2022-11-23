23d1e60c60cccc3b6bf327d45cd329b7736a3e6f
Alex Deweert <adeweert@artandlogic.com>
2022-11-23 07:05:23 -0700

    added rudimentary keypress, not fully working

--- pages/play.tsx
+++ pages/play.tsx
@@ -1,23 +1,86 @@
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { Key, useEffect, useState } from "react";
 import { interval, Subscription } from "rxjs";
 // import styles from "../styles/Home.module.css";
 
 export default function Play() {
-  const timer = interval(128);
+  // const timer = interval(128);
+  const timer = interval(1000);
   const sub = new Subscription();
+  const [cell0Class, setCell0Class] = useState('blue-cell')
+  const [keyPressed, setKeyPressed] = useState(false)
+  const [keyReleased, setKeyRelease] = useState(true)
+  const [activeCell, setActiveCell] = useState([0,0])
+
+
+  let handleKeyPressed = (event: any) => {
+    if(event.key) {
+      event.preventDefault();
+      if(!keyPressed) {
+        console.log(`~~ PRESSED ${event.key}, keyPressed: ${keyPressed}`)
+        // console.log(`active cell WAS ${activeCell}`)
+        if(event.key === 'w') {
+          console.log('UP')
+          // setActiveCell([activeCell[0], activeCell[1]++])
+          setKeyPressed(true)
+        }
+        else if(event.key === 'a') {
+          console.log('LEFT')
+          setKeyPressed(true)
+        }
+        else if(event.key === 's') {
+          console.log('DOWN')
+          setKeyPressed(true)
+        }
+        else if(event.key === 'd') {
+          console.log('RIGHT')
+          setKeyPressed(true)
+        }
+        console.log(`~~ keyPressed: ${keyPressed}`)
+        // console.log(`active cell IS ${activeCell}`)
+      }
+    }
+  }
+
+  let handleKeyRelease = (event: any) => {
+    if(event.key) {
+      event.preventDefault();
+      console.log(`~~ RELEASED`)
+      if(event.key === 'w') {
+        setKeyPressed(false)
+      }
+      else if(event.key === 'a') {
+        setKeyPressed(false)
+      }
+      else if(event.key === 's') {
+        setKeyPressed(false)
+      }
+      else if(event.key === 'd') {
+        setKeyPressed(false)
+      }
+    }
+  }
 
   //Crude gameloop.
   //7.5 fps is 128ms interval
   useEffect(() => {
+    console.log("triggered")
     sub.add(
       timer.subscribe((i) => {
-        console.log(`~~~ tick: ${i}`);
+        console.log(`~~~ tick: ${i} cell0Class: ${cell0Class}`);
+        setCell0Class( i % 2 == 0 ? 'red-cell' : 'blue-cell')
       })
     );
 
+    if(undefined !== document) {
+      document.addEventListener('keyup', handleKeyRelease)
+      document.addEventListener('keydown', handleKeyPressed)
+    }
+
     return () => {
       sub.unsubscribe();
+      document.removeEventListener('keyup', handleKeyRelease);
+      document.removeEventListener('keydown', handleKeyPressed);
     };
   }, []);
   // useEffect(() => {
@@ -43,7 +106,7 @@ export default function Play() {
    //<h1>{router.query.a}</h1>
   return (
     <div className="parent">
-      <div className="cell0 cell blue-cell">cell0</div>
+      <div className={`cell0 cell ${cell0Class}`}>cell0</div>
       <div className="cell1 cell red-cell">cell1</div>
       <div className="cell2 cell blue-cell">cell2</div>
       <div className="cell3 cell red-cell">cell3</div>
